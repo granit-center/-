78a39670fa2956120a5cc077d4495395f907f9ba
Sergei Krupenko <krupenkosm@bk.ru>
2021-11-02 13:07:22 +0300

    d/z

---
+++ public/front.php
@@ -0,0 +1,22 @@
+<?php
+require_once("../vendor/autoload.php");
+
+use Symfony\Component\HttpFoundation\Request;
+ use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+
+$request = Request::createFromGlobals();
+$routes = include("src/app.php");
+$context = new Routing\RequestContext();
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
+$response->send();
+
--- public/page/hello.php
+++
@@ -1,4 +0,0 @@
-
-<?php
-$name = $request->get('name', 'World');
-$response->setContent(sprintf('hello %s',htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
